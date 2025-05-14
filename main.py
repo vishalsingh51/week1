de3f67e5dd69fbc58e3a7e330df9642fb42eddb1
user.name <vishalkumarsingh5155@gmail.com>
2025-05-15 00:27:15 +0530

    Input

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("hello")
-
-print("hello2")
+n=str(input("What is your name?"))
+a=str(input("Hello "+n+ " how are you?"))
+p=str(input("Where are you live?"))
+s=str(input("How is the weather in "+p+"?"))
+w=str(input("What is your age?"))
+print("Good to know you are "+w+ " year old!")
+l=int(input("length of your house?"))
+b=int(input("breadth of your house?"))
+area=l*b
+print("Area of your house is ",area)
+f=int(input("Radius of your football ground?"))
+r=3.14*f*f
+print("Area of your football ground is ",r)
