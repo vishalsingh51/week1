03183f20cef8f8f5f1b06da72af5636ac2c69c10
user.name <vishalkumarsingh5155@gmail.com>
2025-05-15 01:19:51 +0530

    datatypes

---
+++ datatype.py
@@ -0,0 +1,16 @@
+n=10
+print(n)
+print("10 is of type ",type(n))
+r=6.7
+print(r)
+print("6.7 is of type ",type(r))
+x='vishal'
+print(x)
+print("vishal is of type ",type(x))
+l=[10,20,30,40]
+print(l)
+print("l is of type ",type(l))
+b1 = True #'T' has to be capital
+b2 = False #'F' has to be capital
+print(type(b1))
+print(type(b2))
